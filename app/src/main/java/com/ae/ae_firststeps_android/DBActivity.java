767c0844140ba52fdd9de52c92663100d547f093
Archie1983 <archie1983@hotmail.co.uk>
2019-05-08 22:50:26 +0100

    Attempt to demonstrate data base (SQLite) functionality- at the moment still buggy.

--- app/src/main/java/com/ae/ae_firststeps_android/DBActivity.java
+++ app/src/main/java/com/ae/ae_firststeps_android/DBActivity.java
@@ -1,11 +1,16 @@
 package com.ae.ae_firststeps_android;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class DBActivity extends AppCompatActivity {
 
@@ -25,6 +30,39 @@ public class DBActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClickAddName(View view) {
+        // Add a new student record
+        ContentValues values = new ContentValues();
+        values.put(StudentsProvider.NAME,
+                ((EditText)findViewById(R.id.txtName)).getText().toString());
+
+        values.put(StudentsProvider.GRADE,
+                ((EditText)findViewById(R.id.txtGrade)).getText().toString());
+
+        Uri uri = getContentResolver().insert(
+                StudentsProvider.CONTENT_URI, values);
+
+        Toast.makeText(getBaseContext(),
+                uri.toString(), Toast.LENGTH_LONG).show();
+    }
+    public void onClickRetrieveStudents(View view) {
+        // Retrieve student records
+        String URL = "content://com.ae.ae_firststeps_android.StudentsProvider";
+
+        Uri students = Uri.parse(URL);
+        Cursor c = managedQuery(students, null, null, null, "name");
+
+        if (c.moveToFirst()) {
+            do{
+                Toast.makeText(this,
+                        c.getString(c.getColumnIndex(StudentsProvider._ID)) +
+                                ", " +  c.getString(c.getColumnIndex( StudentsProvider.NAME)) +
+                                ", " + c.getString(c.getColumnIndex( StudentsProvider.GRADE)),
+                        Toast.LENGTH_SHORT).show();
+            } while (c.moveToNext());
+        }
     }
 
 }
